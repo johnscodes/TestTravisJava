300be4dc726356d83074a807c975281ea896ab93
John <johnsprogramcodes@gmail.com>
2020-07-21 05:02:14 -0700

    Update HelloWorld.java

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -1,9 +1,9 @@
-package helloWorld;
+package helloworld;
 
 public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		logger.log("Hello World!");
 	}
 }
